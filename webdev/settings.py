25325f86cb0970145d8e9099dd5211cace4d7deb
Raí <raicarvalho343@gmail.com>
2025-08-14 16:57:27 -0300

    Ajustando o settings, para o css

--- webdev/settings.py
+++ webdev/settings.py
@@ -117,8 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/') # NECESSARIO PARA O DEPLOY
-STATIC_URL = [
+STATICFILES_DIR = [
     BASE_DIR / 'static/',
     ]
 
